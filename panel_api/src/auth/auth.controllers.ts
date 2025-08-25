8e4a8157bfbcb8d6313f84ff2a2b807902ec2370
Eduardo Chacon <eduardochacon194@gmail.com>
2025-08-25 10:05:06 -0600

    Commit
    It was install prisma, with docker compose

--- panel_api/src/auth/auth.controllers.ts
+++ panel_api/src/auth/auth.controllers.ts
@@ -1,4 +1,4 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, Logger } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 
 @Controller('auth')
@@ -6,7 +6,9 @@ export class AuthController {
     constructor(private authService: AuthService) {}
     
     @Post('login')
-    signin() {
-        return this.authService.singin();
+    signin(@Body() body: any) {
+        Logger.log(body.EmailId);
+        return `Hello, ${body.EmailId || 'World'}!`;
+        // return this.authService.singin();
     }
 }
